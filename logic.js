23547b9674c127affda8fdf7cff924408867a10b
Ryan Gautier <ryangautier2@gmail.com>
2020-03-27 13:24:47 -0500

    fixing url and api search

--- logic.js
+++ logic.js
@@ -1,28 +1,35 @@
 
-var url = "hhttps://api.nytimes.com/svc/search/v2/articlesearch.json";
-var searchTerm = $("#searchTerm").val();
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 var apiKey = "&api-key=wDG96UJnAnivLybkePfjNNA0CNJ1dhrm";
 var createRow = function (data) {
-  var topArticles = $("articles");
+    var topArticles = $("articles");
 }
 
 
-// Start and end date must be a string in the format yyyymmdd
-var startDate = "&begin_date=" + $("#startYear").val() + "0101";
-var endDate = "&end_date=" + $("#endYear").val() + "0101";
 
-
-// combine all terms into final query URL
-var queryUrl = url + searchTerm + startDate + endDate + apiKey;
 
 // Test API key, returns articles about Obama between 01/01/2008 and 06/01/2008
 // var queryUrl = url + "Obama" + "&begin_date=20080101" + "&end_date=20080601" + apiKey;
 
 $("#search").on("click", function (event) {
     event.preventDefault();
-    console.log(startDate);
-    console.log(endDate);
-    console.log(searchTerm);
+
+    var searchTerm = $("#searchTerm").val();
+
+    // Start and end date must be a string in the format yyyymmdd
+    var start = $("#startDate").val();
+    var end = $("#endDate").val();
+
+    // removing dashes and putting date in yyyymmdd
+    start = start[0] + start[1] + start[2] + start[3] + start[5] + start[6] + start[8] + start[9];
+    var startDate = "&begin_date=" + start;
+    
+    end = end[0] + end[1] + end[2] + end[3] + end[5] + end[6] + end[8] + end[9];
+    var endDate = "&end_date=" + end;
+
+    // combine all terms into final query URL
+    var queryUrl = url + searchTerm + startDate + endDate + apiKey;
+
     $.ajax({
         url: queryUrl,
         method: "GET"
